0c056e6b24ef0c492b8b5782b36274a80114d297
Geunsu Ryu <linusdamyo@gmail.com>
2022-03-30 19:58:48 +0900

    mysql 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'ajdajdsiasia',
+      database: 'todo_test',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
